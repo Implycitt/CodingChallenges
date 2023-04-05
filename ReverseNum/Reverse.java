d413033dba1c2e72513cd7eec4ca7e85f43a0861
Implycitt <qgbordelon@gmail.com>
2023-04-04 19:18:04 -0500

    Added ReverseNumber Class

--- ReverseNum/Reverse.java
+++ ReverseNum/Reverse.java
@@ -1,8 +1,31 @@
 package ReverseNum;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Reverse {
     public static void main(String[] args) {
-        System.out.println("im tired");
-    }
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int num = input.nextInt();
+        System.out.println(concat(reverse(num)));
+        input.close();
+  }
 
+  private static ArrayList<Integer> reverse(int num) {
+    ArrayList<Integer> stack = new ArrayList<>();
+    while(num > 0) {
+      stack.add(num%10);
+      num = num / 10;
+    }
+    return stack;
+  }
+
+  private static String concat(ArrayList<Integer> stack) {
+    StringBuilder sb = new StringBuilder(stack.size());
+    for (int digit : stack) {
+        sb.append(digit);
+    }
+    return sb.toString();
+  }
 }
